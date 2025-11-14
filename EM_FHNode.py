ff011be77a0997b1bcf689f858d2b7bc43c4af66
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-11-14 12:22:32 -0500

    Updated copyright headers

--- EM_FHNode.py
+++ EM_FHNode.py
@@ -1,31 +1,28 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 # SPDX-FileNotice: Part of the ElectroMagnetic addon.
 
-#***************************************************************************
-#*                                                                         *
-#*   Copyright (c) 2018                                                    *
-#*   Efficient Power Conversion Corporation, Inc.  http://epc-co.com       *
-#*                                                                         *
-#*   Developed by FastFieldSolvers S.R.L. under contract by EPC            *
-#*   http://www.fastfieldsolvers.com                                       *
-#*                                                                         *
-#*   This program is free software; you can redistribute it and/or modify  *
-#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
-#*   as published by the Free Software Foundation; either version 2 of     *
-#*   the License, or (at your option) any later version.                   *
-#*   for detail see the LICENCE text file.                                 *
-#*                                                                         *
-#*   This program is distributed in the hope that it will be useful,       *
-#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-#*   GNU Library General Public License for more details.                  *
-#*                                                                         *
-#*   You should have received a copy of the GNU Library General Public     *
-#*   License along with this program; if not, write to the Free Software   *
-#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
-#*   USA                                                                   *
-#*                                                                         *
-#***************************************************************************
+################################################################################
+#                                                                              #
+#   © 2018 Efficient Power Conversion Corporation, Inc. ( http://epc-co.com )  #
+#                                                                              #
+#   Developed by FastFieldSolvers S.R.L. ( http://www.fastfieldsolvers.com )   #
+#   under contract by Efficient Power Conversion Corporation, Inc.             #
+#                                                                              #
+#   This addon is free software: you can redistribute it and/or modify it      #
+#   under the terms of the GNU Lesser General Public License as published      #
+#   by the Free Software Foundation, either version 2.1 of the License,        #
+#   or (at your option) any later version.                                     #
+#                                                                              #
+#   This addon is distributed in the hope that it will be useful,              #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty                #
+#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    #
+#   See the GNU Lesser General Public License for more details.                #
+#                                                                              #
+#   You should have received a copy of the GNU Lesser                          #
+#   General Public License along with this addon.                              #
+#   If not, see https://www.gnu.org/licenses                                   #
+#                                                                              #
+################################################################################
 
 
 __title__="FreeCAD E.M. Workbench FastHenry Node Class"
